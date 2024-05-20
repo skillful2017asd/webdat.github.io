2bd4ef1ca016bb13646e8446218fa252441d4f92
Dat <dh52110775@student.stu.edu.vn>
2024-05-20 13:28:13 +0700

    hi

--- scripts/login.js
+++ scripts/login.js
@@ -7,29 +7,27 @@ signupLink.onclick = () => {
 
 const closeBtnForm = document.querySelector(".form-close");
 closeBtnForm.onclick = () => {
-  let inputsRegisterForm = document.querySelectorAll(
-    "#signUp-container .form-group input"
-  )
   document.querySelector("#signUp-container").style.display = "none"
-  document.querySelector("#register-form").classList.remove("was-validated")
-  inputsRegisterForm.forEach((input)=> {
-    input.value = ""; 
-});
-}
+};
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-function test() {
-  'use strict'
-  let forms = document.querySelectorAll('.needs-validation')
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms)
-    .forEach(function (form) {
-      form.addEventListener('submit', function (event) {
-        form.classList.add('was-validated')
-        if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
-        }
-      }, true)
-    })
-}
+const toggleForm = () => {
+  const closeBtnForm = document.querySelector(".form-close");
+  closeBtnForm.onclick = () => {
+      document.querySelector("#signUp-container").style.display = "none";
+      const inputsSignUpForm = document.querySelectorAll(
+          "#signUp-container [name][rules]"
+      );
+      for (var input of inputsSignUpForm) {
+          input.value = "";
+          var formGroup = input.closest(".form-group");
+          if (formGroup.classList.contains("invalid")) {
+              formGroup.classList.remove("invalid");
+              let formMessage = formGroup.querySelector(".form-message");
+              if (formMessage) formMessage.innerText = "";
+          }
+      }
+  };
+  document.querySelector(".switch-register-form").onclick = () => {
+      document.querySelector("#signUp-container").style.display = "flex";
+  };
+};
