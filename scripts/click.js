7fe8a12429d1bc8b584323b0607cf7149ad58cb6
Dat <dh52110775@student.stu.edu.vn>
2024-05-22 19:31:07 +0700

    commit

--- scripts/click.js
+++ scripts/click.js
@@ -3,7 +3,6 @@ function openNav() {
     document.getElementById("mySidebar").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
     document.querySelector(".openbtn").style.display = "none"
-
 }
 
 /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
@@ -12,3 +11,17 @@ function closeNav() {
     document.getElementById("main").style.marginLeft = "0";
     document.querySelector(".openbtn").style.display = "flex";
 }
+let btnBar = document.querySelector('.btn-bar');
+let menuNgang = document.querySelector('.menu_ngang');
+let close = document.querySelector('.close-bar');
+
+close.onclick =()=>{
+    menuNgang.classList.remove('d-b');
+}
+btnBar.onclick = ()=>{
+    menuNgang.classList.add('d-b');
+}
+
+
+
+
