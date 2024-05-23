b0fa6fc23158f672e2199115a6f275c07ec89e84
Dat <dh52110775@student.stu.edu.vn>
2024-05-23 23:27:21 +0700

    hello

--- scripts/check.js
+++ scripts/check.js
@@ -6,6 +6,7 @@ $('.form-submit').click(function (event){
     let pass = $('input[name="passwordRegister"]').val();
 
 
+
     let validateName = true;
 	let validatePass = true;
 	let validateEmail = true;
@@ -45,8 +46,80 @@ $('.form-submit').click(function (event){
         $('.errorpass').text('');
         validateName = true;
     }
+
     validate = validateName && validatePass && validateEmail && validatePhone;
     if(validate){
         alert('Đăng ký thành công')
     }
+});
+
+$('.form-submit-login').click(function(e){
+    let email_login = $('input[name="emailLogin"]').val();
+    let pass_login = $('input[name="passwordLogin"]').val();
+
+    let validatePass_login = true;
+	let validateEmail_login = true;
+
+    if (email_login === ''){
+        $('.erroremail_login').text('Email đang để trống');
+        validateEmail_login = false
+    }else{
+        $('.erroremail_login').text('');
+        validateEmail_login = true;
+    }
+    if (pass_login === ''){
+        $('.errorpass_login').text('Mật khẩu đang để trống');
+        validatePass_login= false
+    }else{
+        $('.errorpass_login').text('');
+        validatePass_login = true;
+    }
+
+});
+
+$('.form-contact').click(function(e){
+    let email_contact = $('input[name="email-Contact"]').val();
+    let name = $('input[name="name-Contact"]').val();
+    let prolem = $('input[name="problem-Contact"]').val();
+    let content = $('input[name="content-Contact"]').val();
+
+    let validateName = true;
+	let validateProblem = true;
+	let validateEmail = true;
+    let validateContent = true;
+    let validate = true;
+
+    if(email_contact ===''){
+        $('.erroremail_contact').text('Email đang để rỗng');
+        validateEmail = false;
+    }else{
+        $('.erroremail_contact').text('aaa');
+        validateEmail= true;
+    }
+    if(name ===''){
+        $('.errorname').text('Tên đang để rỗng');
+        validateName = false;
+    }else{
+        $('.errorname').text('');
+        validateName = true;
+    }
+    if(prolem ===''){
+        $('.errorproblem').text('Vấn đề đang để rỗng');
+        validateProblem = false;
+    }else{
+        $('.errorproblem').text('');
+        validateProblem = true;
+    }
+    if(content ===''){
+        $('.errorcontent').text('Nội dung đang để rỗng');
+        validateProblem = false;
+    }else{
+        $('.errorcontent').text('');
+        validateProblem = true;
+    }
+    validate = validateEmail && validateName && validateProblem&& validateContent;
+    if(validate){
+        alert('Cảm ơn quý khách đã góp ý')
+    }
+
 });
--- scripts/login.js
+++ scripts/login.js
@@ -9,25 +9,3 @@ const closeBtnForm = document.querySelector(".form-close");
 closeBtnForm.onclick = () => {
   document.querySelector("#signUp-container").style.display = "none"
 };
-
-const toggleForm = () => {
-  const closeBtnForm = document.querySelector(".form-close");
-  closeBtnForm.onclick = () => {
-      document.querySelector("#signUp-container").style.display = "none";
-      const inputsSignUpForm = document.querySelectorAll(
-          "#signUp-container [name][rules]"
-      );
-      for (var input of inputsSignUpForm) {
-          input.value = "";
-          var formGroup = input.closest(".form-group");
-          if (formGroup.classList.contains("invalid")) {
-              formGroup.classList.remove("invalid");
-              let formMessage = formGroup.querySelector(".form-message");
-              if (formMessage) formMessage.innerText = "";
-          }
-      }
-  };
-  document.querySelector(".switch-register-form").onclick = () => {
-      document.querySelector("#signUp-container").style.display = "flex";
-  };
-};
