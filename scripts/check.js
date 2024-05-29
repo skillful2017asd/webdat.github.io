41a856792c70a38b15bd17a934c0d6be96de319f
Dat <dh52110775@student.stu.edu.vn>
2024-05-29 19:18:30 +0700

    xong

--- scripts/check.js
+++ scripts/check.js
@@ -1,5 +1,5 @@
 
-$('.form-submit').click(function (event){
+$('.form-submit').click(function (event) {
     let email = $('input[name="emailRegister"]').val();
     let firstname = $('input[name="firstName"]').val();
     let phone = $('input[name="phoneNumber"]').val();
@@ -8,128 +8,139 @@ $('.form-submit').click(function (event){
 
 
     let validateName = true;
-	let validatePass = true;
-	let validateEmail = true;
-	let validatePhone = true;
-	let validate = true;
+    let validatePass = true;
+    let validateEmail = true;
+    let validatePhone = true;
+    let validate = true;
 
-    // if (!validateEmail(email)) {
-    //     $('.erroremail').text('email đang không đúng ');
-    //     validateEmail = false
-    // } else {
-    //     $('.erroremail').text('');
-    //     validateEmail = true;
-    // }
-    if (email === ''){
+    if (email === '') {
         $('.erroremail').text('email đang để trống');
         validateEmail = false
-    }else{
+    } else if (!isValidEmail(email)) {
+        $('.erroremail').text('Email không hợp lệ');
+        validateEmail = false;
+    }
+    else {
         $('.erroremail').text('');
         validateEmail = true;
     }
-    if (firstname === ''){
+    if (firstname === '') {
         $('.errorname').text('Tên đang để trống');
         validateName = false
-    }else{
+    } else {
         $('.errorname').text('');
         validateName = true;
     }
-    if (phone === ''){
+    if (phone === '') {
         $('.errorphone').text('Số điện thoại đang để trống');
         validateName = false
+    } else if (!isValidPhone(phone)) {
+        $('.errorphone').text('Số điện thoại không hợp lệ');
+        validatePhone = false;
     }
-    else if  (isNaN(phone)){
-        $('.errorpass').text('Hãy nhập số điện thoại là kiểu số');
-        validateName = false
-    }
-    else{
+    else {
         $('.errorphone').text('');
         validateName = true;
     }
-    if (pass === ''){
+    if (pass === '') {
         $('.errorpass').text('Mật khẩu đang để trống');
         validateName = false
-     }else{
+    } else {
         $('.errorpass').text('');
         validateName = true;
     }
-    // function validateEmail(email) {
-    //     var emailRegex = /\S+@\S+\.\S+/;
-    //     return emailRegex.test(email);
-    // }
-
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    function isValidPhone(phone) {
+        const phoneRegex = /^\+?\d{1,3}?[-.\s]?\(?\d{1,3}\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}$/;
+        return phoneRegex.test(phone);
+    }
     validate = validateName && validatePass && validateEmail && validatePhone;
-    if(validate){
+    if (validate) {
         alert('Đăng ký thành công')
     }
 });
 
-$('.form-submit-login').click(function(e){
+$('.form-submit-login').click(function (e) {
     let email_login = $('input[name="emailLogin"]').val();
     let pass_login = $('input[name="passwordLogin"]').val();
 
     let validatePass_login = true;
-	let validateEmail_login = true;
+    let validateEmail_login = true;
 
-    if (email_login === ''){
+    if (email_login === '') {
         $('.erroremail_login').text('Email đang để trống');
         validateEmail_login = false
-    }else{
+    }
+    else if (!isValidEmail(email)) {
+        $('.erroremail_login').text('Email không hợp lệ');
+        validateEmail = false;
+    } else {
         $('.erroremail_login').text('');
         validateEmail_login = true;
     }
-    if (pass_login === ''){
+    if (pass_login === '') {
         $('.errorpass_login').text('Mật khẩu đang để trống');
-        validatePass_login= false
-    }else{
+        validatePass_login = false
+    } else {
         $('.errorpass_login').text('');
         validatePass_login = true;
     }
 
 });
 
-$('.form-contact').click(function(e){
+$('.form-contact').click(function (e) {
     let email_contact = $('input[name="email-Contact"]').val();
     let name = $('input[name="name-Contact"]').val();
     let prolem = $('input[name="problem-Contact"]').val();
     let content = $('input[name="content-Contact"]').val();
 
     let validateName = true;
-	let validateProblem = true;
-	let validateEmail = true;
+    let validateProblem = true;
+    let validateEmail = true;
     let validateContent = true;
     let validate = true;
 
-    if(email_contact ===''){
+    if (email_contact === '') {
         $('.erroremail_contact').text('Email đang để rỗng');
         validateEmail = false;
-    }else{
-        $('.erroremail_contact').text('aaa');
-        validateEmail= true;
     }
-    if(name ===''){
+    else if (!isValidEmail(email)) {
+        $('.erroremail_contact').text('Email không hợp lệ');
+        validateEmail = false;
+    } else {
+        $('.erroremail_contact').text('aaa');
+        validateEmail = true;
+    }
+    if (name === '') {
         $('.errorname').text('Tên đang để rỗng');
         validateName = false;
-    }else{
+    } else {
         $('.errorname').text('');
         validateName = true;
     }
-    if(prolem ===''){
+    if (prolem === '') {
         $('.errorproblem').text('Vấn đề đang để rỗng');
         validateProblem = false;
-    }else{
+    } else {
         $('.errorproblem').text('');
         validateProblem = true;
     }
-    if(content ===''){
+    if (content === '') {
         $('.errorcontent').text('Nội dung đang để rỗng');
         validateProblem = false;
-    }else{
+    } else {
         $('.errorcontent').text('');
         validateProblem = true;
     }
-    validate = validateEmail && validateName && validateProblem&& validateContent;
-    if(validate){
+    function isValidEmail(email) {
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return emailRegex.test(email);
+    }
+    validate = validateEmail && validateName && validateProblem && validateContent;
+    if (validate) {
         alert('Cảm ơn quý khách đã góp ý')
     }
 
